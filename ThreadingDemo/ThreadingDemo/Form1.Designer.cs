4347040723cc95e78026fc42c1be87186ae11c86
Blake Hoelle <bhoelle216@gmail.com>
2018-11-28 21:23:42 -0500

    Commit

---
+++ ThreadingDemo/ThreadingDemo/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ThreadingDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTimeConsumingWork = new System.Windows.Forms.Button();
+            this.btnPrintNumbers = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTimeConsumingWork
+            // 
+            this.btnTimeConsumingWork.Location = new System.Drawing.Point(12, 248);
+            this.btnTimeConsumingWork.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTimeConsumingWork.Name = "btnTimeConsumingWork";
+            this.btnTimeConsumingWork.Size = new System.Drawing.Size(191, 28);
+            this.btnTimeConsumingWork.TabIndex = 0;
+            this.btnTimeConsumingWork.Text = "Do Time Consuming Work";
+            this.btnTimeConsumingWork.UseVisualStyleBackColor = true;
+            this.btnTimeConsumingWork.Click += new System.EventHandler(this.btnTimeConsumingWork_Click);
+            // 
+            // btnPrintNumbers
+            // 
+            this.btnPrintNumbers.Location = new System.Drawing.Point(12, 284);
+            this.btnPrintNumbers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPrintNumbers.Name = "btnPrintNumbers";
+            this.btnPrintNumbers.Size = new System.Drawing.Size(191, 28);
+            this.btnPrintNumbers.TabIndex = 1;
+            this.btnPrintNumbers.Text = "Print Numbers";
+            this.btnPrintNumbers.UseVisualStyleBackColor = true;
+            this.btnPrintNumbers.Click += new System.EventHandler(this.btnPrintNumbers_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(191, 212);
+            this.listBox1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(216, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.clearToolStripMenuItem.Text = "&Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 324);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnPrintNumbers);
+            this.Controls.Add(this.btnTimeConsumingWork);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTimeConsumingWork;
+        private System.Windows.Forms.Button btnPrintNumbers;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ThreadingDemo/ThreadingDemo/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; // MUST USE SYSTEM.THREADING
+
+namespace ThreadingDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DoMoreTimeConsumingWork()
+        {
+            Thread.Sleep(3500); // Mili - Seconds
+        }
+
+        private void DoTimeConsumingWork()
+        {
+            Thread.Sleep(30); // Mili - Seconds
+        }
+
+        private void btnTimeConsumingWork_Click(object sender, EventArgs e)
+        {
+            /******Spam The print numbers button to watch  the click event
+                       execute only after the thread completes******/
+
+            //btnTimeConsumingWork.Enabled = false;
+            //btnPrintNumbers.Enabled = false;
+
+            DoMoreTimeConsumingWork();
+
+            //btnTimeConsumingWork.Enabled = true;
+            //btnPrintNumbers.Enabled = true;
+        }
+
+        private void btnPrintNumbers_Click(object sender, EventArgs e)
+        {
+            btnTimeConsumingWork.Enabled = false;
+            btnPrintNumbers.Enabled = false;
+
+            for (int i = 100; i >= 1; i--)
+            {
+                DoTimeConsumingWork();
+                listBox1.Items.Add(i);
+                Application.DoEvents(); //Application.DoEvents() is nessessary to push numbers 
+                //Individually instead of all at once... Comment out the line & watch the difference.
+            }
+
+            btnTimeConsumingWork.Enabled = true;
+            btnPrintNumbers.Enabled = true;
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
